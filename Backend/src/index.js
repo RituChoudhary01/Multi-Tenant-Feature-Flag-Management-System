33b30a53e1fb64ae018dc2915986b9eb1b9d7412
ritudigident-jpg <ritudigident@gmail.com>
2026-06-25 12:19:03 +0530

    update in code

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -7,7 +7,32 @@ import flagRoute from "./routes/flag.routes.js"
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 4000;
-app.use(cors());
+const allowedOrigins = [
+  "https://multi-tenant-feature-flag-managemen-neon.vercel.app",
+  "https://multi-tenant-feature-flag-managemen-one.vercel.app",
+  "https://multi-tenant-feature-flag-managemen-seven.vercel.app",
+  "http://localhost:5173", 
+  "http://localhost:3000"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error(`CORS blocked for origin: ${origin}`));
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 app.use('/api/auth',authRoute);
 app.use('/api/organizations',OrganizationsRoute);
